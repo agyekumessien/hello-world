0e4bd20c38a73a40d8535e92deebaddcc90f981f
Peter Essien <agyekumessien@yahoo.com>
2021-06-11 17:21:16 -0400

    insert update my files

---
+++ ArrayBasics/src/UsingArray.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+
+
+public class UsingArray {
+	public static void main (String[] args) {
+		
+		//Aray of Bytes
+		//How do I define my variable?
+		byte prepopArray [] = {1, 2};
+		
+		System.out.println(prepopArray.length);
+		
+		
+		//First Item in the Array
+		System.out.println(prepopArray[0]);
+		
+		
+		//last Item in the Array
+				System.out.println(prepopArray[1]);
+				
+				int indexOfLastItem = prepopArray.length - 1;
+		
+				
+				char charElement = (char)prepopArray[indexOfLastItem];
+				System.out.print(charElement);
+				
+				
+				String[] array = new String[4];
+				System.out.print(charElement);
+				
+				System.out.print(array[0]);
+				
+				
+				//int x
+				//System.out.print
+	}
+	
+
+}
